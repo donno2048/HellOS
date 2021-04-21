49bf2df09795aa3f6c8305a1f180e40ffe33ba47
Elisha Hollander <just4now666666@gmail.com>
2021-04-22 01:36:43 +0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,15 +1,14 @@
 org 7c00h
-start:
-	hello db 48h, 65h, 6ch, 6ch, 6fh, 21h, 0
-	cli
-	mov si, hello
-	mov ah, 0eh
-	.loop:
-		lodsb
-		or al, al
-		jz halt
-		int 10h
-		jmp .loop
+hello db 48h, 65h, 6ch, 6ch, 6fh, 21h, 0
+cli
+mov si, hello
+mov ah, 0eh
+.loop:
+	lodsb
+	or al, al
+	jz halt
+	int 10h
+	jmp .loop
 halt hlt
 times (200h + $$ - 2 - $) db 0
 dw 0xaa55
